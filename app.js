909d32e6d45f148b7cee7b27851e21ceb28d1582
muscaiu <muscaiu@yahoo.com>
2016-10-21 09:32:21 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+(function(){
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBbSh3rx0APy-rqM8bbyQC0XuM1-s6UoWs",
+    authDomain: "ionic-chat-32977.firebaseapp.com",
+    databaseURL: "https://ionic-chat-32977.firebaseio.com",
+    storageBucket: "ionic-chat-32977.appspot.com",
+    messagingSenderId: "18119206165"
+  };
+  firebase.initializeApp(config);
+
+}())
